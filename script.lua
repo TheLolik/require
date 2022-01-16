b60667a5b1fb223d0682c441afa19676e3e38c24
TheLolik <79652963+TheLolik@users.noreply.github.com>
2022-01-16 12:45:45 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,8 @@
-print("ok")
+getgenv().import = function(file)
+	local s,e = pcall(function() game:HttpGet("https://raw.githubusercontent.com/TheLolik/require/main/libraryes/"..file..".lua") end)
+	if s then
+		return loadstring(game:HttpGet("https://raw.githubusercontent.com/TheLolik/require/main/libraryes/"..file..".lua"))()
+	elseif not s then
+		warn(e)
+	end
+end
